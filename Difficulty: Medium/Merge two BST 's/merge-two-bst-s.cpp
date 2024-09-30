8a0361f10b6144282f459c743792743d6748f36f
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-09-30 22:20:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -96,61 +96,34 @@ class Solution {
   public:
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
+    void inorderTraversal(Node* root,vector<int>&v1)
+    {
+       if(root==NULL)return;
+       inorderTraversal(root->left,v1);
+       v1.push_back(root->data);
+       inorderTraversal(root->right,v1);
+    }
     vector<int> merge(Node *root1, Node *root2) {
-        vector<int> ans;
-        stack<Node*> st1,st2;
-        while(1)
+        // Your code here
+        vector<int>first,second,ans;
+        inorderTraversal(root1,first);
+        inorderTraversal(root2,second);
+        int i=0,j=0;
+        while(i<first.size() && j<second.size())
         {
-            while(root1)
-            {
-                st1.push(root1);
-                root1=root1->left;
-            }
-            
-            while(root2)
-            {
-                st2.push(root2);
-                root2=root2->left;
-            }
-            
-            if(st1.empty() && st2.empty()) break;
-            
-            Node *curr1 = nullptr, *curr2 = nullptr;
-            if(!st1.empty())
-                curr1 = st1.top();
-            
-            if(!st2.empty())
-                curr2 = st2.top();
-            
-            
-            if(curr1 && curr2){
-                if(curr1 -> data <= curr2 -> data){
-                    ans.push_back(curr1 -> data);
-                    curr1 = curr1 -> right;
-                    st1.pop();
-                    root1 = curr1;
-                }
-                else{
-                    ans.push_back(curr2 -> data);
-                    curr2 = curr2 -> right;
-                    st2.pop();
-                    root2 = curr2;
-                }
-            }   
-            else if(curr1){
-                ans.push_back(curr1 -> data);
-                st1.pop();
-                curr1 = curr1 -> right;
-                root1 = curr1;
-            }
-            else{
-                ans.push_back(curr2 -> data);
-                st2.pop();
-                curr2 = curr2 -> right;
-                root2 = curr2;
-            }
+            if(first[i]<=second[j])ans.push_back(first[i++]);
+            else ans.push_back(second[j++]);
+        }
+        while(i<first.size())
+        {
+            ans.push_back(first[i++]);
+        }
+        while(j<second.size())
+        {
+            ans.push_back(second[j++]);
         }
         return ans;
+        
     }
 };
 
