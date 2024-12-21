251ae5211caea10c8095b1428a219b0579c9eaac
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-12-22 00:36:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -7,29 +7,38 @@ using namespace std;
 class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
-        int n=a.size(),m=b.size();
-        int i=0,j=0;
-        while(i<n && j<m && k>0)
-        {
-            if(k==1) return min(a[i],b[j]);
-            if(a[i]<b[j]) i++;
-            else j++;
-            k--;
+        int n = a.size();
+        int m = b.size();
+    
+        // Ensure a is the smaller array
+        if (n > m) {
+            return kthElement(b,a,k);
         }
-        while(i<n && k>0)
-        {
-            if(k==1) return a[i];
-            i++;
-            k--;
+    
+        int low=max(0,k-m),high=min(k,n);
+    
+        while (low<=high) {
+            int cut1=(low+high)/2;
+            int cut2=k-cut1;
+    
+            int left1=(cut1==0)?INT_MIN:a[cut1-1];
+            int left2=(cut2==0)?INT_MIN:b[cut2-1];
+            int right1=(cut1==n)? INT_MAX:a[cut1];
+            int right2=(cut2==m)? INT_MAX:b[cut2];
+    
+            if (left1<=right2&&left2<=right1) {
+                return max(left1,left2);
+            } else if (left1>right2) {
+                high=cut1-1;
+            } else {
+                low=cut1+1;
+            }
         }
-        while(j<m && k>0)
-        {
-            if(k==1) return b[j];
-            j++;
-            k--;
-        }
+    
+        return -1; // If the input is invalid
     }
 };
+
 
 //{ Driver Code Starts.
 
