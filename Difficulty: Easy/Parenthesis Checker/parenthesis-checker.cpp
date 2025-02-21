806dc0a8359f7dfd0d3b9266c25d17a61a755d88
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-02-21 15:56:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -1,25 +1,22 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to check if brackets are balanced or not.
-    bool ispar(string x)
-    {
+class Solution {
+  public:
+    bool isBalanced(string& s) {
         stack <char> st;
-        int n = x.length();
+        int n = s.length();
         for(int i = 0; i < n; i++){
-            if(x[i] == '(' || x[i] == '{' || x[i] == '[') st.push(x[i]);
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
             else {
                 if(st.empty()) return false;
-                if ((x[i] == ')' && st.top() != '(') ||
-                    (x[i] == '}' && st.top() != '{') ||
-                    (x[i] == ']' && st.top() != '[')) {
+                if ((s[i] == ')' && st.top() != '(') ||
+                    (s[i] == '}' && st.top() != '{') ||
+                    (s[i] == ']' && st.top() != '[')) {
                     return false;
                 }
                 st.pop();
@@ -27,24 +24,24 @@ class Solution
         }
         return st.empty();
     }
-
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-   int t;
-   string a;
-   cin>>t;
-   while(t--)
-   {
-       cin>>a;
-       Solution obj;
-       if(obj.ispar(a))
-        cout<<"balanced"<<endl;
-       else
-        cout<<"not balanced"<<endl;
-   }
+int main() {
+    int t;
+    string a;
+    cin >> t;
+    while (t--) {
+        cin >> a;
+        Solution obj;
+        if (obj.isBalanced(a))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        cout << "~"
+             << "\n";
+    }
 }
 // } Driver Code Ends
