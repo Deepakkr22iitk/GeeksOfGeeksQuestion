0706e57d837ed5feb9c7c44d634d7c9d8bc3fee4
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-04-24 00:10:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number III/unique-number-iii.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int getSingle(vector<int> &arr) {
+        int N=arr.size();
+        unordered_map<int,int> freq;
+        for(int i=0;i<N;i++)
+        {
+            freq[arr[i]]++;
+        }
+        for(auto &it:freq)
+        {
+            if (it.second == 1) return it.first;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSingle(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
