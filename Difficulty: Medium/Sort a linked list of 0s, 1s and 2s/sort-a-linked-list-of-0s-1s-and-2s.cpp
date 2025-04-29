7d274f81689cf5c93d3d344cd92e51ac5d9edfc4
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-04-29 20:31:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+
+// } Driver Code Ends
+
+/*  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        int z=0;
+        int o=0;
+        int t=0;
+        Node *node=head;
+        while(node!=NULL){
+            if(node->data==0){
+                z+=1;
+            }
+            else if(node->data==1){
+                o+=1;
+            }
+            else{
+                t+=1;
+            }
+            node=node->next;
+        }
+        node=head;
+        while(node!=NULL){
+            if(z>0){
+                node->data=0;
+                z-=1;
+            }
+            else if(o>0){
+                node->data=1;
+                o-=1;
+            }
+            else{
+                node->data=2;
+                
+            }
+            node=node->next;
+        }
+        return head;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+    int t, k;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        if (ss >> x) {
+            head = new Node(x);
+            tail = head;
+
+            while (ss >> x) {
+                tail->next = new Node(x);
+                tail = tail->next;
+            }
+
+            // Link the last node to the head if k is 1 to make it circular
+            if (k == 1) {
+                tail->next = head;
+            }
+        }
+
+        Solution ob;
+        Node* newHead = ob.segregate(head);
+        printList(newHead);
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
