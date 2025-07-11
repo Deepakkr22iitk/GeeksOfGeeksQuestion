feb99e7dfe63b3e7d4986123331c8b800b7f3fe1
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-07-11 22:21:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int fib(int n){
+      int first=1,second=1;
+      int sum=0;
+      for(int i=3;i<=n;i++){
+          sum=first+second;
+          first=second;
+          second=sum;
+      }
+      return sum;
+  }
+    int countConsec(int n) {
+        return (1<<n) - fib(n+2);
+    }
+};
