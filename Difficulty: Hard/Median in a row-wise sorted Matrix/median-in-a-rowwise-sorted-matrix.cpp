acffb0c935edc7fd0234689d30c8a7e13c58f3f2
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-08-22 11:07:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,86 +1,38 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    int pred(vector<vector<int>> &mat,int val)
-    {
-        int m=mat.size();
-        int n=mat[0].size();
-        
-        int res=0;
-        
-        for(int i=0;i<m;++i)
-        {
-            int low=0,high=n-1;
-            int ans=m;
-            while(low<=high)
-            {
-                int mid=low+(high-low)/2;
-                if(mat[i][mid]>val)
-                {
-                    ans=mid;
-                    high=mid-1;
-                }
-                else low=mid+1;
-            }
-            res+=ans;
-        }
-        return res;
-    }
     int median(vector<vector<int>> &mat) {
-        int m=mat.size();
-        int n=mat[0].size();
+        int n=mat.size();
+        int m=mat[0].size();
         
-        int req=(m*n)/2;
-        
-        int low=INT_MAX,high=0;
-        for(int i=0;i<m;++i)
-        {
-            low=min(low,mat[i][0]);
-            high=max(high,mat[i][n-1]);
+        int minVal=INT_MAX, maxVal=INT_MIN;
+  
+        // finding the minimum and maximum elements
+        // in the matrix
+        for(int i = 0;i<n;i++) {
+            if (mat[i][0]<minVal)
+                minVal=mat[i][0];
+            if (mat[i][m-1]>maxVal)
+                maxVal=mat[i][m-1];
         }
-
-        while(low<=high)
-        {
+        
+        int desired=(n*m+1)/2;
+    	int low=minVal, high=maxVal;
+        while (low<high) {
             int mid=low+(high-low)/2;
+            int place=0;
+    
+            // count elements smaller than or equal to mid
+            for (int i=0;i<n;++i)
+                place+=upper_bound(mat[i].begin(),mat[i].end(),mid)
+                         -mat[i].begin();
             
-            if(pred(mat,mid)<=req) low=mid+1;
-            else high=mid-1;
+            // adjust the range based on the count of 
+            // elements found
+            if (place<desired)
+                low=mid+1;
+            else
+                high=mid;
         }
-        
         return low;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int r, c;
-        cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c));
-        for (int i = 0; i < r; ++i)
-            for (int j = 0; j < c; ++j)
-                cin >> matrix[i][j];
-        Solution obj;
-        int ans = -1;
-        ans = obj.median(matrix);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
