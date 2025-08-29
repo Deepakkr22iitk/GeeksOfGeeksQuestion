9a0bfac2f14984862444e5c1f0b4ac33b591dd57
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-08-30 00:53:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,53 +1,24 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& mat) {
+    int celebrity(vector<vector<int>>& mat) {
         int n=mat.size();
-        int res = -1;
-        int count1 = 0;
-        int count2 = 0;
-        for(int i=0 ; i<n ; i++){
-            for (int j=0 ; j<n ; j++){
-                    count1+=mat[i][j];
-                    count2+=mat[j][i];
+        vector<int> known_by(n);
+        for(int j=0;j<n;++j)
+        {
+            int sum=0;
+            for(int i=0;i<n;++i)
+            {
+                sum+=mat[i][j];
             }
-            if (count1 == 0 && count2==n-1){
-                res = i;
-            }
-            count1=0;
-            count2=0;
+            known_by[j]=sum;
         }
-        return res;
+        for(int i=0;i<n;++i)
+        {
+            if(accumulate(mat[i].begin(),mat[i].end(),0)==1 && known_by[i]==n)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int> > M(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.celebrity(M) << endl;
-    }
-}
-
-// } Driver Code Ends
