563771064e182ab4867bf5c2ef370ee3ab09d764
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-10-22 21:40:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -1,63 +1,33 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
     void nearlySorted(vector<int>& arr, int k) {
-        int n = arr.size();
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-
-        for (int i = 0; i < n; ++i) {
-            minHeap.push(arr[i]);
-
-            // If heap size exceeds k, extract the minimum element
-            if (minHeap.size() > k) {
-                arr[i - k] = minHeap.top();
-                minHeap.pop();
-            }
+        int n=arr.size();
+        
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        // pushing first k elements in pq
+        for (int i = 0; i < k; i++)
+            pq.push(arr[i]);
+    
+        int i;
+    
+        for (i = k; i < n; i++) {
+    
+            pq.push(arr[i]);
+    
+            // size becomes k+1 so pop it
+            // and add minimum element in (i-k) index
+            arr[i - k] = pq.top();
+            pq.pop();
         }
-
-        // Update the remaining elements
-        int j = n - k;
-        while (!minHeap.empty()) {
-            arr[j++] = minHeap.top();
-            minHeap.pop();
+    
+        // puting remaining elements in array
+        while (!pq.empty()) {
+            arr[i - k] = pq.top();
+            pq.pop();
+            i++;
         }
-
+        
         return ;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution obj;
-        obj.nearlySorted(arr, k);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        // cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
