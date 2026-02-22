c84d4c2cab040876c2d41e6822f7f4c63666f5ad
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-02-23 01:46:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -1,24 +1,13 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template in C++
-
 class Solution {
   public:
-    // Function to return the count of number of elements in union of two arrays.
-    int findUnion(vector<int>& a, vector<int>& b) {
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
         set<int> present;
-        int ans=0;
+        vector<int> ans;
         for(int i=0;i<a.size();++i)
         {
             if(present.find(a[i])==present.end())
             {
-                ans++;
+                ans.push_back(a[i]);
                 present.insert(a[i]);
             }
         }
@@ -26,46 +15,10 @@ class Solution {
         {
             if(present.find(b[i])==present.end())
             {
-                ans++;
+                ans.push_back(b[i]);
                 present.insert(b[i]);
             }
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after reading t
-
-    while (t--) {
-        vector<int> a;
-        vector<int> b;
-
-        string input;
-        // For a
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        // For b
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            b.push_back(number);
-        }
-
-        Solution ob;
-        cout << ob.findUnion(a, b) << endl;
-        cout << '~' << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
