37f0edb8e29723b5bf374176b3ab9bf2edd26162
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-02-22 22:55:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     long subarrayXor(vector<int> &arr, int k) {
@@ -34,34 +28,3 @@ class Solution {
         return ans; // Return the total count
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore(); // Ignore the newline after the number of test cases
-
-    while (tc-- > 0) {
-        string input;
-        getline(cin, input); // Read the whole line for the array
-
-        stringstream ss(input);
-        vector<int> arr;
-        int num;
-
-        while (ss >> num) {
-            arr.push_back(num); // Push numbers into the vector
-        }
-
-        int k;
-        cin >> k;
-        cin.ignore(); // Ignore the newline after k
-
-        Solution obj;
-        cout << obj.subarrayXor(arr, k) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
