2724acaff1f680ff7aaef1583524e61e03dec260
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-04-15 21:14:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string URLify(string &s) {
+        string ans="";
+        
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == ' '){
+                ans+="%20";
+            }
+            else {
+         ans+=s[i];
+    
+    }    
+    
+            
+        }
+        return ans;
+    }
+};
