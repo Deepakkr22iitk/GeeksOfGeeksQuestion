68f27e04ddb4ca73f68d604364161b64def0f62c
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-04-16 22:43:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implement Atoi/implement-atoi.cpp
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -1,46 +1,16 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int myAtoi(char *s) {
-        string str(s);
-        int sign = 1;
-        int index=0;
-        int n = str.size();
-        while(index<n && (str[index]==' '))
-            index++;
-        long long finalResult = 0;
-        for(int i=index;i<n;i++) {
-            if(i==0 && str[i]=='-')
-                sign=-1;
-            else if(str[i]>='0' && str[i]<='9')
-                finalResult=finalResult*10 + (str[i]-'0');
-            else
-                break;
-            if(sign==-1 && -finalResult<INT_MIN)
-                return INT_MIN;
-            if(sign==1 && finalResult>INT_MAX)
-                return INT_MAX;
+    int myAtoi(string &s) {
+        int sign = 1, r = 0, i = 0;
+        while(s[i] == ' ')      i++;        // ignore whitespaces
+        if(s[i] == '-' || s[i] == '+'){
+            if(s[i++] == '-')       sign = -1;
         }
-        return sign*finalResult;
+        while(s[i] >= '0' && s[i] <= '9'){ 
+            if(r > INT_MAX/10 || r == INT_MAX/10 && s[i]-'0' > 7)     
+                return      sign == 1 ? INT_MAX : INT_MIN;
+            r = 10*r+(s[i++]-'0');
+        }
+        return  r*sign;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        char s[20];
-        cin >> s;
-        Solution ob;
-        int ans = ob.myAtoi(s);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-}
-// } Driver Code Ends
