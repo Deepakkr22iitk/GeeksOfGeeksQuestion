22bab5a9cd1dda4a420693174720ab11901c66f8
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-08-25 21:03:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Moves to Sort Permutation/minimum-moves-to-sort-permutation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int minMoves(vector<int>& arr) {
+        int n=arr.size(),maxi=0;
+        vector<int>v(n+1,0);
+        for(int i:arr){v[i]+=1+v[i-1];maxi=max(maxi,v[i]);}
+        return n-maxi ;
+    }
+};
