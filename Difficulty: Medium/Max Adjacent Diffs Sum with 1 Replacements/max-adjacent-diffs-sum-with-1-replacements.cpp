d6dc8ff20a70e8fc47b544f97bade030427d0d78
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-09-03 04:29:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Adjacent Diffs Sum with 1 Replacements/max-adjacent-diffs-sum-with-1-replacements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int solve(vector<int> &v,int ind,vector<int> &dp)
+    {
+        int n=v.size();
+        if(ind>=n) return 0;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        int taken=0,not_taken=0;
+        taken=v[ind]+solve(v,ind+2,dp);
+        not_taken=0+solve(v,ind+1,dp);
+        
+        return dp[ind]=max(taken,not_taken);
+    }
+    int maxDiffSum(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1) return 0;
+        // if(n==2) return max(arr[0],arr[1])-1;
+        
+        vector<int> contribution(n,0);
+        contribution[0]=arr[0]-1;
+        contribution[n-1]=arr[n-1]-1;
+        for(int i=1;i<n-1;++i)
+        {
+            contribution[i]=2*(arr[i]-1);
+        }
+        vector<int> dp(n,-1);
+        int ans=solve(contribution,0,dp);
+        return ans;
+    }
+};
