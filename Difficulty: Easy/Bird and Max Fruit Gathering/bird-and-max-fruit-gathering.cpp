0da3d04c23ec94076e68fe25efe4d4f08f9419b9
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-09-04 03:21:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bird and Max Fruit Gathering/bird-and-max-fruit-gathering.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    // int solve(vector<int> &v,int rem,int first, int ind)
+    // {
+    //     if(rem==0) return 0;
+    //     int n=v.size();
+    //     if(ind>=n) return 0;
+        
+    //     if(ind==(n-1))
+    //     {
+    //         if(first==1) return 0;
+    //         else return v[ind];
+    //     }
+    //     int taken=v[ind]+solve(v,rem-1,first,ind+2);
+    //     int not_taken=0+solve(v,rem,first,ind+1);
+        
+    //     return max(taken,not_taken);
+    // }
+    int maxFruits(vector<int>& arr, int m) {
+        int n=arr.size();
+        
+        if(m==n) return accumulate(arr.begin(),arr.end(),0);
+        
+        m%=n;
+        int ans=0;
+        for(int i=0;i<m;++i)
+        {
+            ans+=arr[i];
+        }
+        int now=ans;
+        for(int i=m;i<2*n;++i)
+        {
+            now+=(arr[i%n]-arr[(i-m)%n]);
+            ans=max(ans,now);
+        }
+        // now+=(arr[0]-arr[n-m]);
+        return ans;
+        
+        // return max(arr[0]+solve(arr,m-1,1,2),solve(arr,m,0,1));
+    }
+};
