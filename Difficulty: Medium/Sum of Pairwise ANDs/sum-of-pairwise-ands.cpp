038ecbd78d736481143d781c448d8860b41a827e
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-09-06 01:14:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Pairwise ANDs/sum-of-pairwise-ands.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    long long pairAndSum(vector<int> &arr) {
+        int n=arr.size();
+        long long  ans =0;
+        for(int i=0;i<32;i++)
+        {
+            long long  k=0;
+            for(int j=0;j<n;j++)
+            {
+                //checking no of set bit 
+                if((arr[j]&(1<<i))!=0 )
+                k++;
+            }
+            ans+=(1<<i)*((k)*(k-1))>> 1;
+        }
+        return ans;
+    }
+};
